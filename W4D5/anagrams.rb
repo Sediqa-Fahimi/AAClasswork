c9295b3aec43b28324dd2b67d67051fbbce9c919
Reed Haubenstock <rhaubenstock@users.noreply.github.com>
2020-12-11 14:09:08 -0800

    Add two_sum_problems.rb

--- W4D5/anagrams.rb
+++ W4D5/anagrams.rb
@@ -17,4 +17,21 @@
 # p second_anagram?("gizmo", "sally")    #=> false
 # p second_anagram?("elvis", "lives")    #=> true
 
+# built in sort -> O(N log(N))
+# def third_anagram?(str1, str2)
+#     str1.chars.sort == str2.chars.sort
+# end
 
+# p third_anagram?("gizmo", "sally")    #=> false
+# p third_anagram?("elvis", "lives")    #=> true
+
+#O(N)
+def fourth_anagram?(str1, str2)
+    hash = Hash.new(0)
+    str1.chars.each {|char| hash[char] += 1} #O(N)
+    hash2 = Hash.new(0)
+    str2.chars.each {|char| hash2[char] += 1} #O(N)
+    hash == hash2
+end
+p fourth_anagram?("gizmo", "sally")    #=> false
+p fourth_anagram?("elvis", "lives")    #=> true
