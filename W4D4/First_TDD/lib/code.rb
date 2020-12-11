1a2d29716678df03ec2b09864f87c58d22c9f129
SediqaFahimi <sediqa.fahimi@gmail.com>
2020-12-10 17:28:40 -0800

    Worked on stock_picker

--- W4D4/First_TDD/lib/code.rb
+++ W4D4/First_TDD/lib/code.rb
@@ -28,11 +28,28 @@ def my_transpose(array)
 end
 
 def stock_picker(array)
-    # min price is day to buy'
-    buy_day   = array.index(array.min) unless array.index(array.min) == array.length-1
-    max_price = array[buy_day+1..-1].max
-    return nil if max_price.nil?
-    sell_day  = array.index(max_price)
+    profits = []
+    # for each element in the array :
+    array.each_with_index do |ele1,idx1|
+        array[idx+1..-1].each_with_index do |ele2,idx2|
+            # find the profit between that element and each subsequent element.
+            profit = ele2 - ele1
+            # store that difference,index and other index.
+            profits << [profit,idx1,idx2]
+        end
+    end
+    # find largest psitive difference 
+    largest_profit = profits.max { |a,b| a[0] <=> b[0]}
+        # return the indices
+    # return nil if no profit if found.
+    return nil if largest_profit.nil? || largest_profit[0] <= 0 
+    return [largest_profit[1],largest_profit[2]]
 
-    return [buy_day,sell_day]
+    # min price is day to buy'
+    # buy_day   = array.index(array.min) unless array.index(array.min) == array.length-1
+    # max_price = array[buy_day+1..-1].max
+    # return nil if max_price.nil?
+    # sell_day  = array.index(max_price)
+
+    # return [buy_day,sell_day]
 end
