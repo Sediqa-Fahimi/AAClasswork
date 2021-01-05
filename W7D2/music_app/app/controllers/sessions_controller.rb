a6df9ee79a3e9795d24cae9b8f5d95fee34123dc
James Koenig <Koenig.James@Live.com>
2021-01-05 12:07:46 -0800

    finishing up sessions login work, thanks Sediqagit add .

--- W7D2/music_app/app/controllers/sessions_controller.rb
+++ W7D2/music_app/app/controllers/sessions_controller.rb
@@ -1,2 +1,21 @@
 class SessionsController < ApplicationController
+  def new
+    @user = User.new
+    render :new
+  end
+
+  def create
+    @user = User.find_by_credentials(params[:user][:email],
+                                     params[:user][:password])
+    if @user
+      login(@user)
+      redirect_to user_url(@user)
+    else
+      flash.now[:errors] = ["Invalid input"]
+      render :new
+    end
+  end
+
+  def destroy
+  end
 end
