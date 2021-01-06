de74d0285dfa8e3d98c678291996e4f37d13b511
Edwin <edwinzhou99@gmail.com>
2021-01-06 17:34:34 -0500

    add specs to user add setup to faker and add one validattion

--- W7D3/goal_app/spec/models/user_spec.rb
+++ W7D3/goal_app/spec/models/user_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe User, type: :model do
   it {should validate_presence_of(:password_digest)}
   it {should validate_length_of(:password).is_at_least(6)}
   
-  subject(:user1) {FactoryBot.create(:user)}
+  subject(:a_person) {FactoryBot.create(:user)}
 
   it {should validate_uniqueness_of(:username)}
 
@@ -16,4 +16,21 @@ RSpec.describe User, type: :model do
     end
   end
   
+  describe 'password_encryption' do
+    it 'does not save password to the database' do
+      FactoryBot.create(:user, username: 'Harry Potter')
+      user = User.find_by(username: "Harry Potter")
+      expect(user.password).not_to eq ("password")
+    end
+
+    it 'ensure password is encrypted using BCrypt' do
+      expect(BCrypt::Password).to receive(:create).with ("abcdef")
+      FactoryBot.build(:user, password: "abcdef")
+    end
+  end
+  #implement later
+  # describe 'associations' do
+  #   it {should have_many(:)}
+  # end
+
 end
