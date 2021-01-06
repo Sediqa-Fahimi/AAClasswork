5ad01021ca475548c767d68e6109e40cf126e9e1
Edwin <edwinzhou99@gmail.com>
2021-01-06 18:42:30 -0500

    add reset session token, is password

--- W7D3/goal_app/app/models/user.rb
+++ W7D3/goal_app/app/models/user.rb
@@ -4,8 +4,18 @@ class User < ApplicationRecord
     validates :password, length: {minimum: 6 }, allow_nil: true
 
     after_initialize :ensure_session_token
+
     
     attr_reader :password
+
+    def self.find_by_credentials(username, password)
+        user = User.find_by(username: username)
+        if user && user.is_password?(password)
+            return user
+        else
+            return nil
+        end
+    end
 
    def password=(pw)
     @password = pw
@@ -15,4 +25,15 @@ class User < ApplicationRecord
    def ensure_session_token
     self.session_token ||= SecureRandom::urlsafe_base64
    end
+
+   def reset_session_token!
+    self.session_token = SecureRandom::urlsafe_base64
+    self.save!
+    self.session_token
+   end
+
+   def is_password?(pw)
+        BCrypt::Password.new(self.password_digest).is_password?(pw)
+   end
+
 end
--- W7D3/goal_app/spec/models/user_spec.rb
+++ W7D3/goal_app/spec/models/user_spec.rb
@@ -1,5 +1,6 @@
 require 'rails_helper'
 
+
 RSpec.describe User, type: :model do
   
   it {should validate_presence_of(:username)}
@@ -29,6 +30,44 @@ RSpec.describe User, type: :model do
       FactoryBot.build(:user, password: "abcdef")
     end
   end
+
+  describe 'reset_session_token' do
+    it 'should reset the session token' do
+      old_token = subject.session_token
+      new_token = subject.reset_session_token!
+      expect(old_token).not_to eq(new_token)
+    end
+    it 'should return the new session token' do
+      expect(subject.session_token).not_to be_nil
+    end
+  end
+
+  describe 'is_password?' do
+    it 'return true if the passwords match' do
+      pw = '123456'
+      result = subject.is_password?(pw)
+      expect(result).to be true
+    end
+
+    it 'return false if the passwords match' do
+      pw = 'abcdef'
+      result = subject.is_password?(pw)
+      expect(result).not_to be true
+    end
+  end
+
+  describe "User::find_by_credentials" do
+    it 'should return the user object if found' do
+      test = User.find_by_credentials('Joe', '123456')
+      expect(subject.password_digest).to eq(test.password_digest)
+    end
+
+    it 'should return nil if user not found' do
+      test = User.find_by_credentials('Bob', '123456')
+      expect(test).to be_nil
+    end
+  end
+
   #implement later
   # describe 'associations' do
   #   it {should have_many(:)}
