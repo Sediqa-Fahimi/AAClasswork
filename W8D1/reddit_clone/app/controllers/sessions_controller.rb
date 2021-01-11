69d61b8fe702b617d82133d38261bcdd1df2048b
Trevor Smith <trevor.smith1667@gmail.com>
2021-01-11 09:47:30 -0800

    added user controller/auth/model

--- W8D1/reddit_clone/app/controllers/sessions_controller.rb
+++ W8D1/reddit_clone/app/controllers/sessions_controller.rb
@@ -1,2 +1,24 @@
 class SessionsController < ApplicationController
+
+    def new
+        render :new
+    end 
+
+    def create
+        @user = User.find_by_credentials(
+            params[:user][:username],
+            params[:user][:password])
+        if @user 
+            login!(@user)
+        else 
+            flash.now[:errors] = ["Invalid username or password"]
+            render :new
+        end 
+    end 
+
+    def destroy
+        logout!
+        redirect_to new_session_url
+    end 
+
 end
--- W8D1/reddit_clone/app/controllers/users_controller.rb
+++ W8D1/reddit_clone/app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+    
+    before_action :require_login!, only: [:index, :show]
+
     def new
         @user = User.new
         render :new
@@ -9,8 +12,18 @@ class UsersController < ApplicationController
     end
     def create
         @user = User.new(user_params)
+        if @user.save
+            login!(@user)
+            redirect_to user_url(@user)
+        else 
+            flash.now[:errors] = @user.errors.full_messages
+            render :new
+        end 
     end
+
     def show
+        @user = User.find_by(id: params[:id])
+        render :show
     end
 
     private
