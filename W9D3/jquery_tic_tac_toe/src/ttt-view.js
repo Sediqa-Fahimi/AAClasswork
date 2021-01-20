b7c185651be66a326c9d4d71c92575ae52a84287
Brynn <brynnreginajohnson@gmail.com>
2021-01-20 17:28:04 -0600

    Start makeMove

--- W9D3/jquery_tic_tac_toe/src/ttt-view.js
+++ W9D3/jquery_tic_tac_toe/src/ttt-view.js
@@ -1,14 +1,25 @@
+const Game = require("../tic_tac_toe_node_solution/game");
+
 class View {
   constructor(game, $el) {
+    this.game = game;
     this.setupBoard();
+    this.bindEvents();
 
   }
 
   bindEvents() {
     
+    $('li').on('click', (e) => {
+      this.makeMove($(e.target));    
+    })
   }
 
-  makeMove($square) {}
+  makeMove($square) {
+    let pos = $square.data('pos');
+    this.game.playMove(pos);
+    $square.text(this.game.currentPlayer);
+  }
 
   setupBoard() {
     const $ul = $("<ul class='grid'></ul>");
@@ -31,6 +42,16 @@ class View {
     $('.grid').append($li8);
     const $li9 = $("<li></li>");
     $('.grid').append($li9);
+    
+    $li.data('pos', [0, 0]);
+    $li2.data('pos', [0, 1]);
+    $li3.data('pos', [0, 2]);
+    $li4.data('pos', [1, 0]);
+    $li5.data('pos', [1, 1]);
+    $li6.data('pos', [1, 2]);
+    $li7.data('pos', [2, 0]);
+    $li8.data('pos', [2, 1]);
+    $li9.data('pos', [2, 2]);
     
 
   }
