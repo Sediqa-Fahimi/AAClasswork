ec15e66349a587df77fddd866c3cd8b9ea65198a
SediqaFahimi <sediqa.fahimi@gmail.com>
2021-01-28 12:02:09 -0800

    Worked on action and reducer

---
+++ W10D4/todospart1/frontend/reducers/todos_reducer.js
@@ -0,0 +1,18 @@
+import { RECEIVE_TODOS, RECEIVE_TODO} from './../actions/todo_actions';
+
+const todosReducer = (state = {},action)=>{
+    console.log("We are in todos reducer");
+    Object.freeze(state);
+    const nextState = Object.assign({},state);
+    switch (action.type){
+        case RECEIVE_TODOS:
+            nextState[action.todos.id] = action.todos;
+            return nextState;
+        case RECEIVE_TODO:
+            nextState[action.todo.id] = action.todo;
+            return nextState;
+        default:
+            return state;
+    }
+}
+export default todosReducer;
