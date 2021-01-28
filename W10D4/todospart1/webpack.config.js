dff7581c5c3c28918d7e0700a394e21d3e355ff1
SediqaFahimi <sediqa.fahimi@gmail.com>
2021-01-28 12:33:17 -0800

    Edited filing structure

--- W10D4/todospart1/webpack.config.js
+++ W10D4/todospart1/webpack.config.js
@@ -2,6 +2,7 @@ var path = require('path');
 module.exports = {
     entry: './frontend/todo_redux.jsx', 
     output: {
+        path: path.resolve(__dirname), 
         filename: './bundle.js',     
     },
     module: {
