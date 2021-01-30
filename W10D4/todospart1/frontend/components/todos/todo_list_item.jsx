67a615c0d116d726059412e22fe85fe477086ff2
SediqaFahimi <sediqa.fahimi@gmail.com>
2021-01-29 17:00:21 -0800

    Finshed deleting the todos via functional component

--- W10D4/todospart1/frontend/components/todos/todo_list_item.jsx
+++ W10D4/todospart1/frontend/components/todos/todo_list_item.jsx
@@ -1,36 +1,39 @@
 import React from 'react';
 
-class TodoListItem extends React.Component{
-  constructor(props){
-    super(props)
-    this.handleClick = this.handleClick.bind(this);
-  }
+// class TodoListItem extends React.Component{
+//   constructor(props){
+//     super(props)
+//     this.handleClick = this.handleClick.bind(this);
+//   }
 
-  handleClick(e){
-    this.props.removeTodo(this.props.todo);
-  }
+//   handleClick(e){
+//     this.props.removeTodo(this.props.todo);
+//   }
 
-  render() {
-    return (
-      <>
-        <li key={this.props.todo.id}>
-          Todo: {this.props.todo.title} {this.props.todo.body}
-        </li>
-        <button onClick={this.handleClick}>Delete</button>
-      </>
-    )
-  }
-}
-
-// const TodoListItem = (props) => {
-//   return (
-//     <>
-//       <li key={props.todo.id}>
-//         Todo: {props.todo.title} {props.todo.body}
-//       </li>
-//       <button onClick={props.removeTodo}>Delete</button>
-//     </>
-//   )
+//   render() {
+//     return (
+//       <>
+//         <li key={this.props.todo.id}>
+//           Todo: {this.props.todo.title} {this.props.todo.body}
+//         </li>
+//         <button onClick={this.handleClick}>Delete</button>
+//       </>
+//     )
+//   }
 // }
+
+const TodoListItem = (props) => {
+  const handleClick=(e)=>{
+    props.removeTodo(props.todo);
+  }
+  return (
+    <>
+      <li key={props.todo.id}>
+        Todo: {props.todo.title} {props.todo.body}
+      </li>
+      <button onClick={handleClick}>Delete</button>
+    </>
+  )
+}
 
 export default TodoListItem;
