5117687beac2f32bb431e391db5a45ceb25ac640
droid4alex <droid4alex@gmail.com>
2021-02-01 15:26:51 -0800

    Finished phase 2 thunk.js

--- W11D1/todos/frontend/util/todo_api_util.js
+++ W11D1/todos/frontend/util/todo_api_util.js
@@ -1,8 +1,8 @@
-
+import { $CombinedState } from 'redux';
 
 export const fetchTodos = ()=>{
     return $.ajax({
         method: 'GET',
         url: '/api/todos'
     })
-}
+} //tested succesffully in browser with fetchTodos().then(res=>console.log(res))
