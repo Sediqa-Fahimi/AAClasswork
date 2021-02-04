440c9aa58762ef59504d1f199730e791759e5a46
SediqaFahimi <sediqa.fahimi@gmail.com>
2021-02-04 11:03:30 -0800

    finished sign up form and container

--- W11D4/frontend_auth/frontend/components/session/signup.jsx
+++ W11D4/frontend_auth/frontend/components/session/signup.jsx
@@ -1,9 +1,42 @@
 import React from 'react';
 
 class Signup extends React.Component{
-    render (){
+    constructor(props){
+        super(props);
+        this.state = {
+            username: '',
+            email: '',
+            password: ''
+        }
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+    handleInput(type){
+        return (e)=>{
+            this.setState({ [type]: e.target.value})
+        }
+    }
+    handleSubmit(e){
+        e.preventDefault();
+        this.props.createNewUser(this.state)
+            .then(()=> this.props.history.push('/chirps'));
+    }
+    render(){
         return (
-            <div></div>
+            <div className="session-form">
+                <h2>Sign Up!</h2>
+                <form>
+                    <label>Username:
+                        <input type="text" value={this.state.username} onchange={this.handleInput('username')}/>
+                    </label>
+                    <label>email:
+                        <input type="text" value={this.state.email} onchange={this.handleInput('email')}/>
+                    </label>
+                    <label>Password:
+                        <input type="password" value={this.state.password} onchange={this.handleInput('password')}/>
+                    </label>
+                    <button onClick={this.handleSubmit}>Sign Up</button>
+                </form>
+            </div>
         );
     }
 };
