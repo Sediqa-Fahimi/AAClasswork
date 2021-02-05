f070fa19dff7f354e121f6e3a5aff4c27caef59c
SediqaFahimi <sediqa.fahimi@gmail.com>
2021-02-05 14:40:46 -0800

    Working on greeting component

--- W11D4/benchbnb/frontend/bench_bnb.jsx
+++ W11D4/benchbnb/frontend/bench_bnb.jsx
@@ -1,16 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {login, logout, signup} from './util/session_api_util';
+import Root from './components/root';
 import configureStore from './store/store';
 
 document.addEventListener("DOMContentLoaded", () => {
-    const root = document.getElementById("root");
-    ReactDOM.render(<h1>Welcome To BenchBNB</h1>, root);
-    window.login = login;
-    window.signup = signup;
-    window.logout = logout;
     const store = configureStore();
+    //testing
     window.getState = store.getState; 
     window.dispatch = store.dispatch; 
+    //
+    const root = document.getElementById("root");
+    ReactDOM.render(<Root store={store}/>, root);
 });
 
