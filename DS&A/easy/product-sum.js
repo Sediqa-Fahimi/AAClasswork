bc6e6ec9f188fef68c28ad0fb7bd9d15cdfb3846
SediqaFahimi <sediqa.fahimi@gmail.com>
2021-05-03 13:49:00 -0700

    add productSum...

--- DS&A/easy/product-sum.js
+++ DS&A/easy/product-sum.js
@@ -1,4 +1,4 @@
-
+//O(n) time | O(d) space , where d is the greatest depth of subArrays.
 function productSum(array, level = 1) {
 	let sum = 0;
 	for(const val of array){
@@ -11,4 +11,3 @@ function productSum(array, level = 1) {
     
 	return sum * level;
 }
-console.log(productSum([5, 2, [7, -1], 3, [6, [-13, 8], 4]]));
