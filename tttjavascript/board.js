3ed59ba4a0523e3255de19c0940147d16173df89
SediqaFahimi <sediqa.fahimi@gmail.com>
2021-05-06 11:59:12 -0700

    create board class

--- tttjavascript/board.js
+++ tttjavascript/board.js
@@ -2,24 +2,26 @@
 class Board{
     constructor(size){
         this.size = size;
-        this.board = Array(size * size).fill(null);
-        
+        // this.board = Array(size * size).fill(null);
+        this.board = Array(size).fill(Array(size));
     }
     won(){
-
+        if(!this.winner()) return false;
+        return true;
     }
     winner(){
-
+        
     }
     empty(pos){
-        return this.board[pos] === null;
+        return this.board[pos[0]][pos[1]] === null;
     }
     place_mark(pos, mark){
         if(!this.empty(pos)) return false;
-        this.board[pos] = mark;
+        this.board[pos[0]][pos[1]] = mark;
         return true; 
     }
 }
 const board = new Board(3);
-console.log(board.board);
 
+board.place_mark([0,1],'X');
+console.log(board.board);
