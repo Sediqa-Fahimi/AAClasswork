92a8e48b0a66567f92d9767dbae654ebf41a119c
SediqaFahimi <sediqa.fahimi@gmail.com>
2021-06-02 15:40:05 -0700

    add constructBST iteratively

--- DS&A/medium/BSTConstruct.js
+++ DS&A/medium/BSTConstruct.js
@@ -1,11 +1,11 @@
-
+//Recursively:
 class BST {
   constructor(value) {
     this.value = value;
     this.left = null;
     this.right = null;
   }
-  
+
   //Average: O(logn) time | O(logn) space
   //Worst: O(n) time | O(n) space
   insert(value) {
@@ -87,5 +87,98 @@ class BST {
 		}else{
 			return this.left.getMinValue();
 		}
+	}
+}
+
+//Iteratively:
+class BST {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+    //Average: O(logn) time | O(1) space
+    //Worst: O(n) time | O(1) space
+  insert(value) {
+    let currentNode = this;
+		while(true){
+			if(value < currentNode.value){
+				if(currentNode.left === null){
+					currentNode.left = new BST(value);
+					break;
+				}else{
+					currentNode = currentNode.left;
+				}
+			}else{
+				if(currentNode.right === null){
+					currentNode.right = new BST(value);
+					break;
+				}else{
+					currentNode = currentNode.right;
+				}
+			}
+		}
+		
+    return this;
+  }
+    //Average: O(logn) time | O(1) space
+    //Worst: O(n) time | O(1) space
+  contains(value) {
+    let currentNode = this;
+		while(currentNode !== null){
+			if(value < currentNode.value){
+				currentNode = currentNode.left;
+			}else if( value > currentNode.value){
+				currentNode = currentNode.right;
+			}else{
+				return true;
+			}
+		}
+		return false;
+  }
+    //Average: O(logn) time | O(1) space
+    //Worst: O(n) time | O(1) space
+  remove(value, parentNode = null) {
+    let currentNode = this;
+		while(currentNode !== null){
+			if(value < currentNode.value){
+				parentNode = currentNode;
+				currentNode = currentNode.left;
+			}else if(value > currentNode.value){
+				parentNode = currentNode;
+				currentNode = currentNode.right;
+			}else{
+				if(currentNode.left && currentNode.right){
+					currentNode.value = currentNode.right.getMinValue();
+					currentNode.right.remove(currentNode.value, currentNode);
+				}else if(parentNode === null){
+					if(currentNode.left){
+						currentNode.value = currentNode.left.value;
+						currentNode.right = currentNode.left.right;
+						currentNode.left = currentNode.left.left;
+					}else if(currentNode.right){
+						currentNode.value = currentNode.right.value;
+						currentNode.left = currentNode.right.left;
+						currentNode.right = currentNode.right.right;
+					}else{
+						//This is a single-node tree; do nothing.
+					}
+				}else if(parentNode.left === currentNode){
+					parentNode.left = currentNode.left !== null ? currentNode.left : currentNode.right;
+				}else if(parentNode.right === currentNode){
+					parentNode.right = currentNode.left !== null ? currentNode.left : currentNode.right;
+				}
+				break;
+			}
+		}
+		
+    return this;
+  }
+	getMinValue(){
+		let currentNode = this;
+		while(currentNode.left){
+			currentNode = currentNode.left;
+		}
+		return currentNode.value;
 	}
 }
