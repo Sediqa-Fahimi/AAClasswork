2992d5b32245a1e39a709449d97950046506eb96
SediqaFahimi <sediqa.fahimi@gmail.com>
2021-06-09 15:35:51 -0700

    add binaryTreeDiameter

--- DS&A/medium/binaryTreeDiameter.js
+++ DS&A/medium/binaryTreeDiameter.js
@@ -11,24 +11,22 @@ class BinaryTree {
 function binaryTreeDiameter(tree) {
   return getTreeInfo(tree).diameter;
 }
-
 function getTreeInfo(tree){
 	if(tree === null){
 		return new TreeInfo(0,0);
 	}
-	
 	const leftTreeInfo = getTreeInfo(tree.left);
 	const rightTreeInfo = getTreeInfo(tree.right);
 	
+	const currentHeight = 1 + Math.max(leftTreeInfo.height, rightTreeInfo.height);
 	const longestPathThroughRoot = leftTreeInfo.height + rightTreeInfo.height;
 	const maxDiameterSoFar = Math.max(leftTreeInfo.diameter, rightTreeInfo.diameter);
 	const currentDiameter = Math.max(longestPathThroughRoot, maxDiameterSoFar);
-	const currentHeight = 1 + Math.max(leftTreeInfo.height , rightTreeInfo.height);
 	
 	return new TreeInfo(currentDiameter, currentHeight);
 }
 
-class TreeInfo{
+class TreeInfo {
 	constructor(diameter, height){
 		this.diameter = diameter;
 		this.height = height;
